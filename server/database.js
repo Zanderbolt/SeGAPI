a17106290d28e76b3d2ebed43251436c0b2120d8
Zanderbolt <christian_renteria@ucol.mx>
2018-11-23 10:18:19 -0600

    Función del API desde Form

--- server/database.js
+++ server/database.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+// const URI = 'mongodb://localhost:27017/'
 const URI = 'mongodb+srv://root:123@segapi-cklce.mongodb.net/test?retryWrites=true'
 
 mongoose.connect(URI)
